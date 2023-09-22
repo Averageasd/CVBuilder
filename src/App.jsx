befdb8a8659ff04ed604fe7bd8bfcf0fc19cfa98
Averageasd <nhuynh9497@gmail.com>
2023-09-21 22:25:55 -0400

    added input examples.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import {InfoSection} from "./components/InfoSection.jsx";
+
 function App() {
-
-  return (
-      <section className="template">
-          <div className="container">
-              <InfoSection></InfoSection>
-          </div>
-      </section>
-
-  )
+    return (
+        <section className="template">
+            <div className="container">
+                <InfoSection infoTitle="General Info"></InfoSection>
+            </div>
+        </section>
+    )
 }
 
 export default App
--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,45 +1,59 @@
 import {useState} from "react";
 import '../GeneralInfo.css';
 
-export function InfoSection() {
+
+export function InfoSection( {infoTitle} ) {
 
     const [nameInput, setNameInput] = useState("");
     const [addressInput, setAddressInput] = useState("");
     const [emailInput, setEmailInput] = useState("");
     const [phoneNumInput, setPhoneNumInput] = useState("");
+    const [inputEditable, setInputEditable] = useState(true);
 
-    const [display, setDisplay] = useState(false);
-
-    function toggleEditable() {
-        setDisplay(!display);
+    function toggleEditable(e) {
+        e.preventDefault();
+        setInputEditable(false);
     }
 
-    
-
+    console.log(inputEditable);
     return (
         <section className="info-section">
             <form className="info-form">
-                <input placeholder="name"
-                       onChange={(e) => setNameInput(e.target.value)}
-                       value={nameInput}>
-                </input>
-                <input placeholder="address"
-                       onChange={(e) => setAddressInput(e.target.value)}
-                       value={addressInput}>
-                </input>
-                <input placeholder="email"
-                       onChange={(e) => setEmailInput(e.target.value)}
-                       value={emailInput}>
-                </input>
-                <input placeholder="phone num"
-                       onChange={(e) => setPhoneNumInput(e.target.value)}
-                       value={phoneNumInput}>
-                </input>
-            </form>
-            <button className="save-info-btn" onClick={toggleEditable}>
-                Save
-            </button>
+                <h2>{infoTitle}</h2>
+                <article className="input-container">
+                    <input placeholder="name"
+                           onChange={(e) => setNameInput(e.target.value)}
+                           contentEditable={inputEditable}
+                           value={nameInput}>
+                    </input>
+                    <p className="input-example">e.g: John Joe.</p>
+                </article>
+                <article className="input-container">
+                    <input placeholder="address"
+                           onChange={(e) => setAddressInput(e.target.value)}
+                           value={addressInput}>
+                    </input>
+                    <p className="input-example">e.g: 1234, Hero Street, My State.</p>
+                </article>
+                <article className="input-container">
+                    <input placeholder="email"
+                           onChange={(e) => setEmailInput(e.target.value)}
+                           value={emailInput}>
+                    </input>
+                    <p className="input-example">e.g: joe151@email.com.</p>
+                </article>
+                <article className="input-container">
+                    <input placeholder="phone num"
+                           onChange={(e) => setPhoneNumInput(e.target.value)}
+                           value={phoneNumInput}>
+                    </input>
+                    <p className="input-example">e.g: John Joe.</p>
+                </article>
 
+                <button type="submit" className="save-info-btn" onClick={toggleEditable}>
+                    Save
+                </button>
+            </form>
         </section>
     )
 }
